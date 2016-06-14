c9c41e9b0f3ece013153a5441467bfe625ad62fb
Pawel Stepniewski <aps015@motorolasolutions.com>
2016-06-14 11:15:27 +0200

    First

--- index.js
+++ index.js
@@ -1,17 +1,19 @@
+"use strict";
 
-console.log("Node app started");
+console.log('Node app started');
 
 
-var express = require("express");
-var app = new express();
+var express = require('express');
+var lodash = require('lodash');
+let app = new express();
 
 app.use(function(req,res,next){
-    console.log("Request ",req.url);
+    console.log('Request ',req.url);
     next();
 });
 
 app.all("/",function(req,res){
-    res.send("Response")
+    res.send('Response ' + (new Date()).toLocaleString())
 });
 
 var port = process.env.PORT || 8080;
